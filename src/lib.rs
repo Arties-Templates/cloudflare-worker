4dd65a9afac0acb06d9f0348d3abce4bc0574623
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-12-11 11:08:40 +1100

    feat: console.error panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
+use console_error_panic_hook::set_once as set_panic_hook;
 use worker::*;
 
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _context: Context) -> Result<Response> {
+    set_console_error_hook();
+
     let router = Router::new();
 
     router
         .get("/", |_, _| Response::ok("owo"))
         .run(req, env)
         .await
+}
+
+fn set_console_error_hook() {
+    set_panic_hook()
 }
